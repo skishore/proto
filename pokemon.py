179c1962a72159e66d8de503671df9610cdc50c8
Shaunak Kishore <skishore@mit.edu>
2013-06-16 17:42:01 -0700

    Add stat formulas

--- pokemon.py
+++ pokemon.py
@@ -3,27 +3,41 @@ from random import randint
 from data import pokedex_data
 from move import Move
 
+level_multiplier = 10
+num_pokemon = 251
+
 
 class Pokemon(object):
-  stats = ('atk', 'def', 'spa', 'spd', 'spe')
+  stats = ('hp', 'atk', 'dfn', 'spa', 'spd', 'spe')
 
   def __init__(self, num, level=1):
     self.num = num
     self.level = level
-    data = pokedex_data[num]
-    self.name = data['name']
-    self.max_hp = data['hp']
-    self.cur_hp = self.max_hp
-    for stat in self.stats:
-      setattr(self, stat, data[stat])
+    self.name = pokedex_data[num]['name']
+    self.compute_stats()
 
-  def apply_noise(self):
+  def compute_stats(self):
+    self.ivs = getattr(self, 'ivs', {})
     for stat in self.stats:
-      setattr(self, stat, getattr(self, stat) + randint(0, 15))
+      self.ivs[stat] = self.ivs.get(stat, randint(0, 15))
+      setattr(self, stat, self.compute_stat(stat))
+    self.max_hp = self.hp
+    self.cur_hp = self.hp
+    del self.hp
+
+  def compute_stat(self, stat):
+    lvl = self.lvl()
+    base_plus_iv = pokedex_data[self.num][stat] + self.ivs[stat]
+    if stat == 'hp':
+      return (base_plus_iv + 50)*lvl/50 + 10
+    return base_plus_iv*lvl/50 + 5
+
+  def lvl(self):
+    return level_multiplier*self.level
 
   @staticmethod
   def random_pokemon():
-    pokemon = Pokemon(randint(1, 251), randint(1, 10))
+    pokemon = Pokemon(randint(1, num_pokemon), randint(1, 2))
     num_moves = randint(3, 4)
     pokemon.moves = Move.random_moves(num_moves)
     return pokemon
