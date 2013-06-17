c7b49c6ad45927a37ea09962acfabceebdea97ae
Shaunak Kishore <skishore@mit.edu>
2013-06-16 17:32:34 -0700

    Add type effectiveness data

--- data.py
+++ data.py
@@ -1,27 +1,38 @@
-types = set([
-  'NORMAL',
-  'FIGHTING',
-  'FLYING',
-  'POISON',
-  'GROUND',
-  'ROCK',
-  'BUG',
-  'GHOST',
-  'STEEL',
-  'FIRE',
-  'WATER',
-  'GRASS',
-  'ELECTRIC',
-  'PSYCHIC',
-  'ICE',
-  'DRAGON',
-  'DARK',
-])
+#------ Parse the type data from types.txt. -------#
+
+num_types = 17
+
+with open('data/types.txt', 'r') as types_txt:
+  raw_type_data = types_txt.read()
+lines = raw_type_data.split('\n')
+assert(len(lines) == 2*num_types + 4)
+
+types = tuple(lines[1:num_types + 1])
+for type in types:
+  assert(type and type.isalnum())
+
+def parse_effectiveness(char):
+  assert(char in (' ', '0', '2') or ord(char) == 189)
+  if ord(char) == 189:
+    return 0.5
+  return {' ': 1, '0': 0, '2': 2}[char]
+
+type_effectiveness = {}
+for (source, line) in zip(types, lines[num_types + 3:]):
+  decoded = line.decode('utf8')
+  assert(len(decoded) == 17)
+  type_effectiveness[source] = {
+    target: parse_effectiveness(char) for (target, char) in zip(types, decoded)
+  }
+  assert(all(type in type_effectiveness[source] for type in types))
+assert(all(type in type_effectiveness for type in types))
+
 
 #------ Parse the Pokemon data from pokemon.txt. -------#
 
 pokedex_data = {}
-raw_pokedex_data = open('data/pokemon.txt', 'r').read()
+with open('data/pokemon.txt', 'r') as pokemon_txt:
+  raw_pokedex_data = pokemon_txt.read()
 
 for (i, line) in enumerate(raw_pokedex_data.split('\r\n')[1:252]):
   row = line.split(',')
@@ -31,7 +42,7 @@ for (i, line) in enumerate(raw_pokedex_data.split('\r\n')[1:252]):
     'name': row[2].upper(),
     'hp': int(row[3]),
     'atk': int(row[4]),
-    'def': int(row[5]),
+    'dfn': int(row[5]),
     'spa': int(row[6]),
     'spd': int(row[7]),
     'spe': int(row[8]),
@@ -53,10 +64,12 @@ def get_front_index(pokenum):
     result += 1
   return result - 1 
 
+
 #------ Parse the move data from my_moves.txt. -------#
 
 move_data = {}
-raw_move_data = open('data/moves.txt', 'r').read()
+with open('data/moves.txt', 'r') as moves_txt:
+  raw_move_data = moves_txt.read()
 
 for line in raw_move_data.split('\r\n')[1:-1]:
   row = line.split(',')
